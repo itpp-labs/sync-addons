eaf9eee6963481158b8dbb0092da257f285cbfcc
Ildar Nasyrov <nasyrov@it-projects.info>
2016-07-18 13:33:54 +0500

    [REF] mailgun: use regular expression in simple security check, don't verify ssl in requests - there is problems with ssl on mailgun

--- mailgun/models.py
+++ mailgun/models.py
@@ -18,7 +18,7 @@ class MailThread(models.AbstractModel):
     @api.model
     def mailgun_fetch_message(self, message_url):
         api_key = self.env['ir.config_parameter'].sudo().get_param('mailgun.apikey')
-        res = requests.get(message_url, headers={'Accept': 'message/rfc2822'}, auth=('api', api_key))
+        res = requests.get(message_url, headers={'Accept': 'message/rfc2822'}, auth=('api', api_key), verify=False)
         self.message_process(False, res.json().get('body-mime'))
 
 
