9ce65a9d1a198a53b5be951a67dcdd15046ef9a7
Ivan Yelizariev <yelizariev@it-projects.info>
2019-08-11 14:53:08 +0000

    :tada::one::zero: upload base_automation_webhook

---
+++ base_automation_webhook/__manifest__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Outgoing Webhooks""",
+    "summary": """Send webhook on Odoo events: when record is created/updated/deleted""",
+    "category": "Extra Tools",
+    # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=10.0",
+    "images": [],
+    "version": "10.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "support": "apps@it-projects.info",
+    "website": "https://apps.odoo.com/apps/modules/10.0/base_automation_webhook/",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "base_action_rule",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+    ],
+    "demo": [
+        "data/base_automation_demo.xml"
+    ],
+    "qweb": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+
+    # "demo_title": "Outgoing Webhooks",
+    # "demo_addons": [
+    # ],
+    # "demo_addons_hidden": [
+    # ],
+    # "demo_url": "DEMO-URL",
+    # "demo_summary": "{SHORT_DESCRIPTION_OF_THE_MODULE}",
+    # "demo_images": [
+    #    "images/MAIN_IMAGE",
+    # ]
+}
