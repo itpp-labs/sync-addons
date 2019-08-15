58e693aea6294108b7f8d6d118eca1b65ecd2de3
Ivan Yelizariev <yelizariev@it-projects.info>
2019-08-15 15:02:30 +0000

    fix importing

--- openapi/__init__.py
+++ openapi/__init__.py
@@ -1,6 +1,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from . import controllers
-from . import models
 
 def post_load():
-    from .controllers import apijsonrequest
+    # make import in post_load to avoid applying monkey patches when this
+    # module is not installed
+    from . import models
+    from . import controllers
