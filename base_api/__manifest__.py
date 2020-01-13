85a2dd250012bb51e805f1229be905fa7d96af06
Anvar Kildebekov <jakildebek@gmail.com>
2020-01-13 16:04:24 +0500

    :one::three::tada: init commit
    :euro: decreased due to creation of base_api
    :book: description of error by skipping step about 'server_wide_modules'
    :rainbow: refactoring due to creation of 'base_api' module
    
    closes #64

---
+++ base_api/__manifest__.py
@@ -0,0 +1,47 @@
+# Copyright 2019 Anvar Kildebekov <https://it-projects.info/team/fedoranvar>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": """Base API""",
+    "summary": """Basic function and methods of API for openapi or XML-RPC""",
+    "category": "Hidden",
+    # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=13.0",
+    "version": "13.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Anvar Kildebekov",
+    "support": "apps@itpp.dev",
+    "website": "https://apps.odoo.com/apps/modules/13.0/base_api/",
+    "license": "LGPL-3",
+    "price": 10.00,
+    "currency": "EUR",
+
+    "depends": [
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+
+    # "demo_title": "Base API",
+    # "demo_addons": [
+    # ],
+    # "demo_addons_hidden": [
+    # ],
+    # "demo_url": "DEMO-URL",
+    # "demo_summary": "basic function and methods of API for openapi or XML-RPC",
+    # "demo_images": [
+    #    "images/MAIN_IMAGE",
+    # ]
+}
