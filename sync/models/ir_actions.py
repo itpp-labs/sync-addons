a7f5797ae805f1d54928defc9eb77aae9d9ce89d
Ivan Yelizariev <yelizariev@itpp.dev>
2020-07-10 17:10:28 +0500

    :tada::one::two: Sync Studio

---
+++ sync/models/ir_actions.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Ivan Yelizariev <https://twitter.com/yelizariev>
+# License MIT (https://opensource.org/licenses/MIT).
+
+from odoo import fields, models
+
+
+class IrActionsServer(models.Model):
+    _inherit = "ir.actions.server"
+
+    sync_task_id = fields.Many2one("sync.task")
+    sync_project_id = fields.Many2one(
+        "sync.project", related="sync_task_id.project_id", readonly=True
+    )
---
+++ sync/models/sync_trigger_automation.py
@@ -0,0 +1,44 @@
+# Copyright 2020 Ivan Yelizariev <https://twitter.com/yelizariev>
+# License MIT (https://opensource.org/licenses/MIT).
+
+from odoo import api, fields, models
+
+
+class SyncTriggerAutomation(models.Model):
+
+    _name = "sync.trigger.automation"
+    _inherit = ["sync.trigger.mixin", "sync.trigger.mixin.actions"]
+    _description = "DB Trigger"
+    _sync_handler = "handle_db"
+    _default_name = "DB Trigger"
+
+    automation_id = fields.Many2one(
+        "base.automation", delegate=True, required=True, ondelete="cascade"
+    )
+
+    def start(self, records):
+        if self.active:
+            self.sync_task_id.start(self, args=(records,))
+
+    def get_code(self):
+        return (
+            """
+env["sync.trigger.automation"].browse(%s).sudo().start(records)
+"""
+            % self.id
+        )
+
+    @api.onchange("model_id")
+    def onchange_model_id(self):
+        self.model_name = self.model_id.model
+
+    @api.onchange("trigger")
+    def onchange_trigger(self):
+        if self.trigger in ["on_create", "on_create_or_write", "on_unlink"]:
+            self.filter_pre_domain = (
+                self.trg_date_id
+            ) = self.trg_date_range = self.trg_date_range_type = False
+        elif self.trigger in ["on_write", "on_create_or_write"]:
+            self.trg_date_id = self.trg_date_range = self.trg_date_range_type = False
+        elif self.trigger == "on_time":
+            self.filter_pre_domain = False
