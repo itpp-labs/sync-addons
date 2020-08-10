beb39a613bc7f2dcf2cfadd33ddba1d43227c42f
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-08-10 20:04:30 +0500

    :ambulance: decode auth header

--- openapi/__manifest__.py
+++ openapi/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "",
     # "live_test_url": "",
     "images": ["images/openapi-swagger.png"],
-    "version": "13.0.1.2.1",
+    "version": "13.0.1.2.2",
     "application": False,
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "support": "apps@itpp.dev",
