1598ae2fd6bce5299c51ef83711479128b262355
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-11-02 22:26:06 +0500

    :cherries:

--- base_automation_webhook/__manifest__.py
+++ base_automation_webhook/__manifest__.py
@@ -9,7 +9,7 @@
     "version": "12.0.2.0.0",
     "application": False,
     "author": "IT-Projects LLC, Ivan Yelizariev",
-    "support": "sync@it-projects.info",
+    "support": "apps@itpp.dev",
     "website": "https://apps.odoo.com/apps/modules/12.0/base_automation_webhook/",
     "license": "Other OSI approved licence",  # MIT
     "price": 60.00,
