6d8d274721a9dcc42d94a0ac4dffe47f302e6a1e
Mitchell Admin <itpp-bot@users.noreply.github.com>
2020-12-26 18:48:12 +0000

    :rainbow: pre-commit > Made via .github/workflows/DINAR-PORT.yml

--- sync/wizard/sync_make_module.py
+++ sync/wizard/sync_make_module.py
@@ -69,11 +69,13 @@ class SyncMakeModule(models.TransientModel):
             self.env["ir.config_parameter"].set_param(PARAM_URL, self.author_url)
 
         url = " <{}>".format(self.author_url) if self.author_url else ""
-        copyright_str = "<!-- Copyright {years} {name}{url}\n     {license_line}. -->".format(
-            years=self.copyright_years,
-            name=self.author_name,
-            url=url,
-            license_line=self.license_line,
+        copyright_str = (
+            "<!-- Copyright {years} {name}{url}\n     {license_line}. -->".format(
+                years=self.copyright_years,
+                name=self.author_name,
+                url=url,
+                license_line=self.license_line,
+            )
         )
         root = etree.Element("odoo")
         project = self.project_id.with_context(active_test=False)
