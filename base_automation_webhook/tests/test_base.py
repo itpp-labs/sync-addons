c8ef5b7b83e739990c34f65397a9e232fb6a328b
Denis Mudarisov <denis.mudarisov@gmail.com>
2021-02-03 22:30:18 +0500

    :shield: update test

--- base_automation_webhook/tests/test_base.py
+++ base_automation_webhook/tests/test_base.py
@@ -1,13 +1,13 @@
 # Copyright 2019 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2021 Denis Mudarisov <https://github.com/trojikman>
 # License MIT (https://opensource.org/licenses/MIT).
 
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase, tagged
 
 
 class TestAutomation(TransactionCase):
-    at_install = True
-    post_install = True
 
+    @tagged("at_install", "post_install")
     def test_requests(self):
         """Check that requests package is available"""
         self.env["res.partner"].create({"name": "New Contact"})
