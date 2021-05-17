2afa5a657d2735b952d6f14054f9fdbbe2d53d8a
Denis Mudarisov <denis.mudarisov@gmail.com>
2021-05-17 14:04:18 +0500

    :book: update module image and icon

--- sync_telegram/__manifest__.py
+++ sync_telegram/__manifest__.py
@@ -5,7 +5,7 @@
     "name": """Telegram Integration""",
     "summary": """Telegram integration powered by Sync Studio""",
     "category": "Discuss",
-    "images": ["images/sync_telegram.png"],
+    "images": ["images/sync_telegram.jpg"],
     "version": "14.0.1.0.0",
     "application": False,
     "author": "IT-Projects LLC, Ilya Ilchenko",
