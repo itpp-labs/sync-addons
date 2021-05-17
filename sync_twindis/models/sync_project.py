b605d18824407b63f9b844e418c13d9b18cb115e
Denis Mudarisov <denis.mudarisov@gmail.com>
2021-05-17 13:08:37 +0500

    :bath: run pre-commit

--- sync_twindis/models/sync_project.py
+++ sync_twindis/models/sync_project.py
@@ -17,24 +17,28 @@ class SyncProjectTwindis(models.Model):
 
     @api.model
     def _eval_context_twindis(self, secrets, eval_context):
-        import requests
         import base64
         import csv
-        from io import BytesIO, TextIOWrapper
         import logging
+        from io import BytesIO, TextIOWrapper
+
+        import requests
+
         _logger = logging.getLogger(__name__)
-        
+
         # log_transmission = eval_context["log_transmission"]
         log = eval_context["log"]
         params = eval_context["params"]
-        if not all([params.PRODUCTS_URL, params.PRICES_URL, secrets.LOGIN, secrets.PASSWORD]):
+        if not all(
+            [params.PRODUCTS_URL, params.PRICES_URL, secrets.LOGIN, secrets.PASSWORD]
+        ):
             raise UserError(_("Credentials are not set"))
 
         def read_csv_attachment(attachment):
             """
             :attachment: ir.attachment record with csv data
 
-            :return: tuple of csv fields and data 
+            :return: tuple of csv fields and data
             """
             decoded_datas = base64.decodebytes(attachment.datas)
             encoding = "utf-8"
@@ -46,7 +50,7 @@ class SyncProjectTwindis(models.Model):
 
         def get_file(url, ref):
             r = requests.get(url, auth=(secrets.LOGIN, secrets.PASSWORD))
-            ref.datas = base64.b64encode((r.content or "\n"))
+            ref.datas = base64.b64encode(r.content or "\n")
             return ref
 
         def fetch_image_from_url(url):
@@ -56,9 +60,14 @@ class SyncProjectTwindis(models.Model):
             """
             data = ""
             try:
-                data = base64.b64encode(requests.get(url.strip()).content).replace(b"\n", b"")
+                data = base64.b64encode(requests.get(url.strip()).content).replace(
+                    b"\n", b""
+                )
             except Exception as e:
-                log("There was a problem requesting the image from URL {}".format(url), level="debug")
+                log(
+                    "There was a problem requesting the image from URL {}".format(url),
+                    level="debug",
+                )
             return data
 
         def create_products(products, group):
@@ -71,13 +80,19 @@ class SyncProjectTwindis(models.Model):
             productgroup = products_fields.index("productgroup")
             for product in products_data:
                 if product[productgroup] == group:
-                    self.env["product.template"].create([{
-                        "name": product[product_name],
-                        "default_code": product[productid],
-                        "image_1920": fetch_image_from_url(product[image_large]),
-                        "list_price": float(product[retail_price]),
-                        "barcode": product[ean]
-                    }])
+                    self.env["product.template"].create(
+                        [
+                            {
+                                "name": product[product_name],
+                                "default_code": product[productid],
+                                "image_1920": fetch_image_from_url(
+                                    product[image_large]
+                                ),
+                                "list_price": float(product[retail_price]),
+                                "barcode": product[ean],
+                            }
+                        ]
+                    )
 
         def create_pricelist(prices):
             prices_fields, prices_data = prices
@@ -87,14 +102,27 @@ class SyncProjectTwindis(models.Model):
             # problem: Lead time is written by default to 1 day.
             # problem: after each run of this function a new record in pricelist is created instead of updated
             for prices in prices_data:
-                if prices[productid] == self.env['product.template'].search([('default_code', '=', prices[productid])]).default_code:
-                    self.env["product.supplierinfo"].create([{
-                        "name": self.env.ref('sync_twindis.res_partner_twindis').id,
-                        "product_code": prices[productid],
-                        "min_qty": prices[min_qty],
-                        "product_tmpl_id": self.env['product.template'].search([('default_code', '=', prices[productid] )]).id,
-                        "price": float(prices[price])
-                    }])
+                if (
+                    prices[productid]
+                    == self.env["product.template"]
+                    .search([("default_code", "=", prices[productid])])
+                    .default_code
+                ):
+                    self.env["product.supplierinfo"].create(
+                        [
+                            {
+                                "name": self.env.ref(
+                                    "sync_twindis.res_partner_twindis"
+                                ).id,
+                                "product_code": prices[productid],
+                                "min_qty": prices[min_qty],
+                                "product_tmpl_id": self.env["product.template"]
+                                .search([("default_code", "=", prices[productid])])
+                                .id,
+                                "price": float(prices[price]),
+                            }
+                        ]
+                    )
 
         return {
             "get_file": get_file,
@@ -102,4 +130,3 @@ class SyncProjectTwindis(models.Model):
             "create_products": create_products,
             "create_pricelist": create_pricelist,
         }
-
