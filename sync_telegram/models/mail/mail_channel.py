c963458c7be5954705360ffbb5548b1fb7368728
Ivan Yelizariev <iel@odoo.com>
2021-07-07 19:48:26 +0200

    :tada: multi_livechat (+refactor sync_telegram)
    the code was previously in sync_telegram

--- sync_telegram/models/mail/mail_channel.py
+++ sync_telegram/models/mail/mail_channel.py
@@ -1,26 +1,12 @@
 # Copyright 2021 Ivan Yelizariev <https://twitter.com/yelizariev>
 # License MIT (https://opensource.org/licenses/MIT).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class MailChannel(models.Model):
     _inherit = "mail.channel"
 
     channel_type = fields.Selection(
-        selection_add=[("telegram", "Telegram Conversation")]
+        selection_add=[("multi_livechat_telegram", "Telegram")],
+        ondelete={"multi_livechat_telegram": "cascade"},
     )
-
-    def _compute_is_chat(self):
-        super(MailChannel, self)._compute_is_chat()
-        for record in self:
-            if record.channel_type == "telegram":
-                record.is_chat = True
-
-    @api.model
-    def channel_fetch_slot(self):
-        values = super(MailChannel, self).channel_fetch_slot()
-        domain = [("channel_type", "=", "telegram")]
-        # pinned_channels = self.env['mail.channel.partner'].search([('partner_id', '=', self.env.user.partner_id.id), ('is_pinned', '=', True)]).mapped('channel_id')
-        # domain += [('id', 'in', pinned_channels.ids)]
-        values["channel_telegram"] = self.search(domain).channel_info()
-        return values
