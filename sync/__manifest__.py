6a94a195fe700f09b604dca18656efd6bde0a5a3
Ilya Ilchenko <ilya@itpp.dev>
2021-08-01 16:04:59 +0300

    :ambulance: sync: add exporting of text parameter values

--- sync/__manifest__.py
+++ sync/__manifest__.py
@@ -8,7 +8,7 @@
     "summary": """Synchronize anything with anything: SystemX↔Odoo, Odoo1↔Odoo2, SystemX↔SystemY. ETL/ESB tool similar to OCA/connector, but more flexible""",
     "category": "Extra Tools",
     "images": ["images/sync-studio.jpg"],
-    "version": "14.0.4.1.1",
+    "version": "14.0.4.1.2",
     "application": True,
     "author": "IT Projects Labs, Ivan Yelizariev",
     "support": "help@itpp.dev",
--- sync/doc/changelog.rst
+++ sync/doc/changelog.rst
@@ -1,4 +1,8 @@
+`4.1.2`
+-------
+
 - **Improvement:** initial values do not overwrite parameter values after a module update.
+- **Fix:** add exporting of text parameter values
 
 `4.1.1`
 -------
