eb20148910bd33bb1ab20b988bcac8c8cf2de8d5
Ivan Yelizariev <iel@odoo.com>
2021-10-01 23:17:50 +0200

    :book: new website

--- base_automation_webhook/__manifest__.py
+++ base_automation_webhook/__manifest__.py
@@ -11,7 +11,7 @@
     "application": False,
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "support": "apps@itpp.dev",
-    "website": "https://apps.odoo.com/apps/modules/10.0/base_automation_webhook/",
+    "website": "https://t.me/sync_studio",
     "license": "Other OSI approved licence",  # MIT
     "depends": ["base_action_rule"],
     "external_dependencies": {"python": [], "bin": []},
