be1fcbc9fb44f207114ada6384b6902ac0a8409e
Ivan Yelizariev <iel@odoo.com>
2021-10-01 22:59:46 +0200

    :book: new website

--- sync_telegram/__manifest__.py
+++ sync_telegram/__manifest__.py
@@ -11,7 +11,7 @@
     "application": False,
     "author": "IT Projects Labs, Ilya Ilchenko",
     "support": "help@itpp.dev",
-    "website": "https://github.com/itpp-labs/sync-addons",
+    "website": "https://t.me/sync_studio",
     "license": "Other OSI approved licence",  # MIT
     "depends": ["sync", "multi_livechat"],
     "external_dependencies": {"python": ["python-telegram-bot"], "bin": []},
