itpp-labs/pos-addons

:ambulance: improved search to take uid from pos_reference

pos_qr_payments/models/pos_order.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from odoo import models, api, fields
+import re
 
 
 class PosOrder(models.Model):
@@ -12,4 +14,5 @@ class PosOrder(models.Model):
     @api.depends('pos_reference')
     def _compute_pos_reference_uid(self):
         for r in self:
-            r.pos_reference_uid = (r.pos_reference or ' ').split(' ', 1)[1]
+            reference = re.search(u'\d{1,}-\d{1,}-\d{1,}', r.pos_reference)
+            r.pos_reference_uid = reference and reference.group(0) or ''

itpp-labs/sync-addons

pos_qr_payments/models/pos_order.py
@@ -1,6 +1,8 @@
 # Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from odoo import models, api, fields
+import re
 
 
 class PosOrder(models.Model):
@@ -11,4 +13,5 @@ class PosOrder(models.Model):
     @api.depends('pos_reference')
     def _compute_pos_reference_uid(self):
         for r in self:
-            r.pos_reference_uid = (r.pos_reference or ' ').split(' ', 1)[1]
+            reference = re.search(r'\d{1,}-\d{1,}-\d{1,}', r.pos_reference)
+            r.pos_reference_uid = reference and reference.group(0) or ''