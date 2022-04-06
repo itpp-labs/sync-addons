4b3110cd2507238ea91b265ca490a8a8e09cb129
Mohammad Tomaraei <mohammed.tomaraei@butopea.com>
2022-04-06 22:12:01 +0200

    added the migration script

--- sync/models/ir_model.py
+++ sync/models/ir_model.py
@@ -6,6 +6,19 @@ from odoo import fields, models
 class IrModelSelection(models.Model):
     _inherit = 'ir.model.fields.selection'
 
+    def _update_sync_project_context(self):
+        spc = self.env['sync.project.context']
+        many2many_values = spc.search([]).mapped(lambda v: v.name)
+        new_selection_values = self.env['ir.model.fields.selection'].search(
+            [('field_id.model', '=', 'sync.project'), ('field_id.name', '=', 'eval_context')]
+        ).filtered(lambda v, vals=many2many_values: v.value not in vals)
+        if new_selection_values:
+            for new_selection_value in new_selection_values:
+                spc.create({
+                    'name': new_selection_value.value,
+                    'display_name': new_selection_value.name
+                })
+
     def _reflect_selections(self, model_names):
         """
             Temporary compatibility helper for modules still using eval_context's selection_add.
@@ -21,16 +34,6 @@ class IrModelSelection(models.Model):
             if field_name == 'eval_context'
         ]
         if eval_context_changed and 'sync.project.context' in self.env:
-            spc = self.env['sync.project.context']
-            many2many_values = spc.search([]).mapped(lambda v: v.name)
-            new_selection_values = self.env['ir.model.fields.selection'].search(
-                [('field_id.model', '=', 'sync.project'), ('field_id.name', '=', 'eval_context')]
-            ).filtered(lambda v, vals=many2many_values: v.value not in vals)
-            if new_selection_values:
-                for new_selection_value in new_selection_values:
-                    spc.create({
-                        'name': new_selection_value.value,
-                        'display_name': new_selection_value.name
-                    })
+            self._update_sync_project_context()
         return res
 
