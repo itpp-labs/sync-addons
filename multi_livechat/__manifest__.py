abfdfb2e4b5304963538e9cd323ec3526548f65d
Mitchell Admin <itpp-bot@users.noreply.github.com>
2022-06-29 08:26:20 +0000

    :arrow_up::one::five: OCA/odoo-module-migrator
    close #416
    
    > Made via .github/workflows/DINAR-PORT.yml

--- multi_livechat/__manifest__.py
+++ multi_livechat/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """Be in touch with your partners via any supported channels (Telegram, WhatsApp, Instragram, etc.)""",
     "category": "Marketing",
     "images": ["images/multi_livechat.jpg"],
-    "version": "14.0.1.2.0",
+    "version": "15.0.1.2.0",
     "application": False,
     "author": "IT Projects Labs, Ivan Yelizariev",
     "support": "help@itpp.dev",
