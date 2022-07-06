02266a643edfc2ecf256bfe9f034d18abc520d70
Ivan Yelizariev <iel@odoo.com>
2022-07-06 17:32:30 +0200

    :arrow_up: multi_livechat, sync_telegram: port to v15 (WIP)

--- multi_livechat/__manifest__.py
+++ multi_livechat/__manifest__.py
@@ -15,15 +15,20 @@
     "depends": ["mail"],
     "external_dependencies": {"python": [], "bin": []},
     "data": ["views/mail_channel_views.xml"],
-    "demo": [],
-    "qweb": [
-        "static/src/components/discuss_sidebar/discuss_sidebar.xml",
-        "static/src/components/discuss_sidebar_item/discuss_sidebar_item.xml",
-    ],
+    "demo": ["data/mail_channel_demo.xml", "data/base_automation_demo.xml"],
     "assets": {
         "web.assets_backend": [
-            "/multi_livechat/static/src/components/discuss_sidebar/discuss_sidebar.js",
-            "/multi_livechat/static/src/models/messaging_initializer/messaging_initializer.js",
+            "multi_livechat/static/src/models/messaging_initializer/messaging_initializer.js",
+            "multi_livechat/static/src/models/discuss/discuss.js",
+            "multi_livechat/static/src/models/messaging_notification_handler/messaging_notification_handler.js",
+            "multi_livechat/static/src/models/thread/thread.js",
+            "multi_livechat/static/src/components/discuss/discuss.js",
+            # demo
+            "multi_livechat/static/src/demo/models/discuss/discuss.js",
+            "multi_livechat/static/src/demo/models/discuss_sidebar_category/discuss_sidebar_category.js",
+        ],
+        "web.assets_qweb": [
+            "multi_livechat/static/src/components/discuss_sidebar/discuss_sidebar.xml",
         ],
     },
     "post_load": None,
