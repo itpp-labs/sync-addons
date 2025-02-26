00f6768cb451d458282fd0f569711d7516002a0f
Mitchell Admin <itpp-bot@users.noreply.github.com>
2025-02-26 08:32:26 +0000

    :arrow_up::one::seven: OCA/odoo-module-migrator
    close #483
    
    > Made via .github/workflows/DINAR-PORT.yml

--- sync/__manifest__.py
+++ sync/__manifest__.py
@@ -7,7 +7,7 @@
     "name": "Sync 🪬 Studio",
     "summary": """Join the Amazing 😍 Community ⤵️""",
     "category": "VooDoo ✨ Magic",
-    "version": "16.0.13.0.1",
+    "version": "17.0.13.0.1",
     "application": True,
     "author": "Ivan Yelizariev",
     "support": "info@odoomagic.com",
