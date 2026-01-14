96c43fa4c89cccafde9a0e79b817870c00eb4e26
Yannik Lieblinger <yannik@lieblinger.de>
2026-01-14 13:35:41 +0100

    [MIG] partner_telegram: Migration to 18.0

--- partner_telegram/__manifest__.py
+++ partner_telegram/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Telegram Contact Field",
     "summary": """Join the Amazing 😍 Community ⤵️""",
     "category": "VooDoo ✨ Magic",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Ivan Kropotkin",
     "support": "info@odoomagic.com",
     "website": "https://sync_studio.t.me/",
